2cfaebe8703ab557dce4235e32fcefaf75461104
Chris <laforet@chrislaforetsoftware.com>
2023-02-21 20:45:56 -0500

    Attempting to create prototype javafx window

--- src/main/java/com/chrislaforetsoftware/logslicer/LogSlicer.java
+++ src/main/java/com/chrislaforetsoftware/logslicer/LogSlicer.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 
 public class LogSlicer extends Application {
 
+    // https://edencoding.com/runtime-components-error/
+
     @java.lang.Override
     public void start(Stage stage) throws Exception {
         final Parent parent = FXMLLoader.load(getClass().getResource("/com/chrislaforetsoftware/logslicer/MainView.fxml"));
