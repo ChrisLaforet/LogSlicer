1f04e26109d56d53bc03fb9d89acbf58e832695f
chris <laforet@chrislaforetsoftware.com>
2023-02-22 19:31:04 -0500

    Add menu item to open file

--- src/main/java/com/chrislaforetsoftware/logslicer/LogSlicer.java
+++ src/main/java/com/chrislaforetsoftware/logslicer/LogSlicer.java
@@ -10,12 +10,19 @@ public class LogSlicer extends Application {
 
     // https://edencoding.com/runtime-components-error/
 
+    private static Stage stage;
+
     @java.lang.Override
     public void start(Stage stage) throws Exception {
         final Parent parent = FXMLLoader.load(getClass().getResource("/com/chrislaforetsoftware/logslicer/MainView.fxml"));
         stage.setTitle("LogSlicer: Parse components from logs");
         stage.setScene(new Scene(parent, 800, 500));
+        LogSlicer.stage = stage;
         stage.show();
+    }
+
+    public static Stage getStage() {
+        return LogSlicer.stage;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/chrislaforetsoftware/logslicer/controller/MainViewController.java
+++ src/main/java/com/chrislaforetsoftware/logslicer/controller/MainViewController.java
@@ -1,10 +1,16 @@
 package com.chrislaforetsoftware.logslicer.controller;
 
+import com.chrislaforetsoftware.logslicer.LogSlicer;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MainViewController {
 
@@ -12,6 +18,21 @@ public class MainViewController {
     private AnchorPane mainContainer;
 
     public void handleOpenLog(ActionEvent actionEvent) {
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open log file");
+        final File file = fileChooser.showOpenDialog(LogSlicer.getStage());
+        if (file != null) {
+            loadFile(file);
+        }
+    }
+
+    private void loadFile(File file) {
+        try {
+
+        } catch (Exception e) {
+
+        } finally {
+        }
     }
 
     public void handleClose(ActionEvent actionEvent) {
