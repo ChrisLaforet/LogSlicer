a8f22c794e895ae2f344edcde578555211b56bce
chris <laforet@chrislaforetsoftware.com>
2023-03-01 17:12:50 -0500

    working towards xml parsing

--- src/main/java/com/chrislaforetsoftware/logslicer/log/LogLine.java
+++ src/main/java/com/chrislaforetsoftware/logslicer/log/LogLine.java
@@ -14,5 +14,7 @@ public class LogLine {
         return this.line;
     }
 
-    public int getLineNumber() { return this.lineNumber; }
+    public int getLineNumber() {
+        return this.lineNumber;
+    }
 }
