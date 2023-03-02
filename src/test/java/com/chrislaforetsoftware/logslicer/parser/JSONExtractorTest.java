1e91889b967b8a4347a009f1dc4bcfdf9ace9d59
Chris <laforet@chrislaforetsoftware.com>
2023-03-02 18:27:54 -0500

    Initial work towards parsing json

--- src/test/java/com/chrislaforetsoftware/logslicer/parser/JSONExtractorTest.java
+++ src/test/java/com/chrislaforetsoftware/logslicer/parser/JSONExtractorTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class JSONExtractorTest {
 
     static private final String SIMPLE_XML = "<Testing></Testing>";
+    static private final String VALID_SINGLE_LINE_JSON = "{\"menu\":{\"id\":\"file\",\"value\":\"File\",\"popup\":{\"menuitem\":[{\"value\":\"New\",\"onclick\":\"CreateNewDoc()\"},{\"value\":\"Open\",\"onclick\":\"OpenDoc()\"},{\"value\":\"Close\",\"onclick\":\"CloseDoc()\"}]}}}";
+    static private final String INVALID_SINGLE_LINE_JSON = "{\"id\":\"file\"  \"value\":\"File\"}";
 
     @Test
     void givenLogLine_whenTextEmpty_thenReturnsNull() {
@@ -55,5 +57,31 @@ class JSONExtractorTest {
         final IMarkupContent json = JSONExtractor.testAndExtractFrom(content, 0);
         assertNotNull(json);
         assertEquals("{}", json.getContent());
+    }
+
+    @Test
+    void givenLogLine_whenContainsValidSingleLineJSON_thenReturnsTag() {
+        final LogContent content = new LogContent();
+        content.addLine(0, VALID_SINGLE_LINE_JSON);
+        final IMarkupContent json = JSONExtractor.testAndExtractFrom(content, 0);
+        assertNotNull(json);
+        assertEquals(VALID_SINGLE_LINE_JSON, json.getContent());
+    }
+
+    @Test
+    void givenLogLine_whenContainsValidSingleLineJSONPrecededByFalseAlarms_thenReturnsTag() {
+        final LogContent content = new LogContent();
+        content.addLine(0, "Testing { With {{ false alarms " + VALID_SINGLE_LINE_JSON + " False }");
+        final IMarkupContent json = JSONExtractor.testAndExtractFrom(content, 0);
+        assertNotNull(json);
+        assertEquals(VALID_SINGLE_LINE_JSON, json.getContent());
+    }
+
+    @Test
+    void givenLogLine_whenContainsMalformedSingleLineJSON_thenReturnsTag() {
+        final LogContent content = new LogContent();
+        content.addLine(0, INVALID_SINGLE_LINE_JSON);
+        final IMarkupContent json = JSONExtractor.testAndExtractFrom(content, 0);
+        assertNull(json);
     }
 }
