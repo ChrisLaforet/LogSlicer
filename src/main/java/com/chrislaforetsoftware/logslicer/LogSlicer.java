89e5a65489ebb1ab0708fe4db6c5b400e8ac54ff
christopher.laforet <christopher.laforet@silverrailtech.com>
2023-03-06 16:09:51 -0500

    temporary check in with arrowfactory example in an attempt to determine how to show xml and json on pertinent lines

--- src/main/java/com/chrislaforetsoftware/logslicer/LogSlicer.java
+++ src/main/java/com/chrislaforetsoftware/logslicer/LogSlicer.java
@@ -2,12 +2,18 @@ package com.chrislaforetsoftware.logslicer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
+
+import java.util.function.IntFunction;
 
 public class LogSlicer extends Application {
 
@@ -22,6 +28,24 @@ public class LogSlicer extends Application {
         stage.setScene(new Scene(parent, 800, 500));
         LogSlicer.stage = stage;
         stage.show();
+
+//CodeArea codeArea = new CodeArea();
+//
+//IntFunction<Node> numberFactory = LineNumberFactory.get(codeArea);
+//IntFunction<Node> arrowFactory = new ArrowFactory(codeArea.currentParagraphProperty());
+//IntFunction<Node> graphicFactory = line -> {
+//    HBox hbox = new HBox(
+//            numberFactory.apply(line),
+//            arrowFactory.apply(line));
+//    hbox.setAlignment(Pos.CENTER_LEFT);
+//    return hbox;
+//};
+//codeArea.setParagraphGraphicFactory(graphicFactory);
+//codeArea.replaceText("The green arrow will only be on the line where the caret appears.\n\nTry it.");
+//codeArea.moveTo(0, 0);
+//
+//stage.setScene(new Scene(new StackPane(codeArea), 600, 400));
+//stage.show();
     }
 
     public static Stage getStage() {
